6543af1defa2f8bceee7782d28e238d1cc6022ca
pbserious <pbserious@gmail.com>
2018-07-09 12:45:41 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			echo "groot!"
-			echo $event['source']
+			echo $event
 			$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
