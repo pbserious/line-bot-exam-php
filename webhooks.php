61c3112ee12853f833c1644743347898a2aff67e
pbserious <pbserious@gmail.com>
2018-07-09 12:44:05 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -16,6 +16,8 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
+			echo "groot!"
+			echo $event['source']
 			$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
