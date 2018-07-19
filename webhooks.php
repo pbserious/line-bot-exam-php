d7583ed343490c294b3832814942b3e1eb6c2129
Rattee Wariyawutthiwat <pbserious@gmail.com>
2018-07-19 19:19:35 +0700

    test imitation

--- webhooks.php
+++ webhooks.php
@@ -18,12 +18,12 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' 
 			&& $event['message']['type'] == 'text') {
 
-			$verifyText = $event['message']['text'];
-			if (strpos($verifyText, 'emji') == false) {
-    			return;
-			}
+			// $verifyText = $event['message']['text'];
+			// if (strpos($verifyText, 'emji') == false) {
+   //  			return;
+			// }
 			// Get text sent
-			$text = "emji is bad guy.";
+			$text = $event['message']['text'];//"emji is bad guy.";
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
